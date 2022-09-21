61f3c847cd659c8ff8d23791ad4e6a6a28e5611a
rahul <esakkanavar98@gmail.com>
2022-09-21 13:28:02 +0530

    product display api

--- server/controller/product.js
+++ server/controller/product.js
@@ -15,7 +15,32 @@ exports.create = async(req,res) =>{
     }
 }
 
-exports.read = async(req,res) =>{
-   let products = await Product.find({});
+exports.listall = async(req,res) =>{
+   let products = await Product.find({})
+   .limit(parseInt(req.params.count))
+   .populate('category')
+   .populate("subs")
+   .sort([["createdAt","desc"]])
+   .exec()
    res.json(products);
+}
+exports.remove = async(req,res) => {
+    console.log("eggsdgsdgsdgdsgds");
+    try{
+        const deleted = await Product.findOneAndRemove({slug:req.params.slug}).exec();
+        console.log(deleted,"aaaaaaaaaaaaaaa")
+        res.json(deleted)
+    }
+    catch(err){
+        console.log(err)
+        return res.status(400).send("product delete failed")
+    }
+}
+
+exports.read = async (req,res) => {
+    const data = await Product.findOne({slug:req.params.slug})
+    .populate('category')
+    .populate('subs')
+    .exec();
+    res.json(data);
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -4,10 +4,12 @@ const router = express.Router();
 //middleware routes
 const { authCheck, adminCheck } = require("../middleware/auth");
 
-const {create,read} = require('../controller/product')
+const {create,listall,remove,read} = require('../controller/product')
 
 router.post("/products", authCheck, adminCheck, create);
-router.get("/products",  read);
+router.get("/products/:count",  listall);
+router.delete("/product/:slug",authCheck,adminCheck,remove)
+router.get("/product/:slug",read)
 
 
 
